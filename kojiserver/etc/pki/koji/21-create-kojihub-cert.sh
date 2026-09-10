6ba5a90f3b04b7adbaccdaca05ac5c94a2372ede
Henryk Paluch <hpaluch@seznam.cz>
2026-09-10 13:12:09 +0200

    koji: web cert must include SAN otherwise koji client will fail

--- kojiserver/etc/pki/koji/21-create-kojihub-cert.sh
+++ kojiserver/etc/pki/koji/21-create-kojihub-cert.sh
@@ -5,8 +5,11 @@ cd $(dirname 0)
 caname=koji
 user=kojihub
 openssl genrsa -out private/${user}.key 2048
-openssl req -config ssl.cnf -new -nodes -out certs/${user}.csr -key private/${user}.key
+# SAN is required for python3.14 SSL support ('koji' client), otherwise it will mercilessly throw error:
+# HTTPSConnectionPool(host='fed-koji.example.com', port=443): Max retries exceeded with url: /kojihub (Caused by SSLError(SSLCertVerificationError(1, "[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: Hostname mismatch, certificate is not valid for 'fed-koji.example.com'. (_ssl.c:1082)")))
+openssl req -config ssl.cnf -new -nodes -out certs/${user}.csr -key private/${user}.key \
+	-addext "subjectAltName = DNS:`hostname -f`"
 openssl ca -config ssl.cnf -keyfile private/${caname}_ca_cert.key -cert ${caname}_ca_cert.crt \
-    -out certs/${user}.crt -outdir certs -infiles certs/${user}.csr
+	-out certs/${user}.crt -outdir certs -infiles certs/${user}.csr
 cat certs/${user}.crt private/${user}.key > ${user}.pem
 exit 0
