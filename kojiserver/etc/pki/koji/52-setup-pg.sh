200fcdb0e1d26d3835875b7b003a8267d2ca0c93
Henryk Paluch <hpaluch@seznam.cz>
2026-09-13 13:16:20 +0200

    koji: fully automate postgres conf patching (listen, and # of conns)

--- kojiserver/etc/pki/koji/52-setup-pg.sh
+++ kojiserver/etc/pki/koji/52-setup-pg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xeuo pipefail
+set -euo pipefail
 
 # allow 'koji' user to connect to postgresql socket
 f=/var/lib/pgsql/data/pg_hba.conf
@@ -8,13 +8,25 @@ cat <<'EOF' > $f
 local   koji        koji                       trust
 local   all         postgres                   peer
 EOF
+cat $f
 
 # disable listening on socket (fixme: expects that there is not yet line listen_addresses in conf!)
 f=/var/lib/pgsql/data/postgresql.conf
-line="listen_addresses = ''"
-grep "/^$line" $f || echo "$line" >> $f
-echo "TODO: Also upper max_connections in $f to at least 200"
 
+sed "/^listen_addresses/s/=.*/= ''/;t;\$a \\\
+listen_addresses = ''" $f > $f.new
+diff -u $f $f.new || {
+	mv -vf $f $f.old
+	mv -v $f.new $f
+}
+
+# Just Apache is using default 100 connections - increased to 200
+sed 's/^\(max_connections *= *\).*/\1200/' $f > $f.new
+diff -u $f $f.new || {
+	mv -vf $f $f.old
+	mv -v $f.new $f
+}
+set -x
 systemctl restart postgresql
 
 # this command should now succeed
