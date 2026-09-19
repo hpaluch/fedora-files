72d208dc3d31d3f3743c41dbcabc95915dcd7fa9
Henryk Paluch <hpaluch@seznam.cz>
2026-09-19 20:06:31 +0200

    fixed build script and my 'upstream' project clockres

--- kojiserver/etc/pki/koji/98-koji-build-clockres-f44.sh
+++ kojiserver/etc/pki/koji/98-koji-build-clockres-f44.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# Build Henryk's clockres project for Fedora44
+# Build Henryk's clockres project for Fedora44 using local Koji installation
 set -euo pipefail
 
 [ `id -u` -ne 0 ] || { echo "ERROR: This script must be run as regular (non-root) user"'!' >&2; exit 1; }
-# note: commit id is latst from branch: rpms/master
-#   --scratch --no-rebuild-srpm
-#   so far it fails, because it runs 'make sources' that is not supported
+# note: commit id is latest from branch: koji/master because Koji requires Makefile with
+#       'sources' target that builds source tarball from git source
 set -x
-
-koji build dist-f44 'git+https://github.com/hpaluch-pil/clockres.git#61c23069efba0e0d03deb806443beb2b972fa225'
+koji list-pkgs --quiet  --tag dist-f44 | grep -w clockres || {
+	# we must add package to distribution - otherwise only --scratch is allowed
+	koji add-pkg --owner kojiadmin dist-f44 clockres
+}
+koji list-pkgs --tag dist-f44
+koji build --draft dist-f44 'git+https://github.com/hpaluch-pil/clockres.git#375260459f49346752ff24d0f7ee2acabdddc442'
 exit 0
